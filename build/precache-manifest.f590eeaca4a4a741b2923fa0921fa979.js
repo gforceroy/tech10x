6e0e4a22eedf01af9ee92ad70cc0ca61cd5ee38b
dbaker <deja.baker@generalassemb.ly>
2020-10-14 18:51:19 -0500

    add build folder

---
+++ build/precache-manifest.f590eeaca4a4a741b2923fa0921fa979.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e7a7644b0b3f495f6db65a4a0f2c75e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eee50e73368614e01cd4",
+    "url": "/static/css/main.e5d0bb9e.chunk.css"
+  },
+  {
+    "revision": "60fca809833e9900aed3",
+    "url": "/static/js/2.e68e58b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e68e58b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eee50e73368614e01cd4",
+    "url": "/static/js/main.8fed1c11.chunk.js"
+  },
+  {
+    "revision": "29d450d6c514390009fb",
+    "url": "/static/js/runtime-main.bda5de2d.js"
+  },
+  {
+    "revision": "9f568237a618b9dbace9434611510912",
+    "url": "/static/media/team-member-01.9f568237.jpg"
+  },
+  {
+    "revision": "5b8d787a87bb3e665e6e22b06af1ca4f",
+    "url": "/static/media/team-member-02.5b8d787a.jpg"
+  },
+  {
+    "revision": "9332992923b6437b35b05f0059e5cd14",
+    "url": "/static/media/team-member-03.93329929.jpg"
+  },
+  {
+    "revision": "5d226ab3eaee4b963b237de6c842dce8",
+    "url": "/static/media/team-mosaic-01.5d226ab3.jpg"
+  },
+  {
+    "revision": "95e7e12a58aab435d6274735a81ab83d",
+    "url": "/static/media/team-mosaic-02.95e7e12a.jpg"
+  },
+  {
+    "revision": "d7fa6cc1be9cabc2476539fb23cf738b",
+    "url": "/static/media/team-mosaic-03.d7fa6cc1.jpg"
+  },
+  {
+    "revision": "6b8f506dd3b16e4b4e59e0f9775e260e",
+    "url": "/static/media/team-mosaic-04.6b8f506d.jpg"
+  }
+]);
